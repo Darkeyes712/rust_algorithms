8a17895c6db3e2f57148b983b83091a8bbb13865
Nikola <nikolakolev712@gmail.com>
2024-07-11 18:52:49 +0300

    add append

--- double_linked_list/src/algorithm.rs
+++ double_linked_list/src/algorithm.rs
@@ -66,9 +66,49 @@ impl<T: std::fmt::Debug + Clone> KolzoDoublyLinkedList<T> {
         }
         println!("None");
     }
-
+    /// Appends a new node with the given value to the end of the doubly linked list.
+    ///
+    /// # Arguments
+    ///
+    /// * `value` - The value to be stored in the new node.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let mut list = KolzoDoublyLinkedList::new();
+    /// list.append(1);
+    /// list.append(2);
+    /// list.append(3);
+    /// assert_eq!(list.length, 3);
+    /// ```
+    ///
+    /// # Safety
+    ///
+    /// This method uses raw pointers to modify the internal structure of the doubly linked list.
+    /// It is marked as `unsafe` because dereferencing raw pointers can lead to undefined behavior
+    /// if not done correctly.
+    ///
+    /// # Panics
+    ///
+    /// This method does not panic.
     pub fn append(&mut self, value: T) {
-        // Some code
+        let new_node = Box::new(Node::new(value));
+        let new_node_ptr: *mut _ = Box::into_raw(new_node);
+
+        unsafe {
+            if self.head.is_none() {
+                self.head = Some(Box::from_raw(new_node_ptr));
+                self.tail = Some(new_node_ptr);
+            } else {
+                if let Some(current) = self.tail {
+                    (*current).next = Some(Box::from_raw(new_node_ptr));
+                    (*new_node_ptr).previous = Some(Box::from_raw(current));
+                    self.tail = Some(new_node_ptr);
+                }
+            }
+
+            self.length += 1;
+        }
     }
 
     pub fn pop(&mut self, value: T) {
@@ -98,4 +138,9 @@ impl<T: std::fmt::Debug + Clone> KolzoDoublyLinkedList<T> {
     pub fn remove(&mut self, value: T) {
         // Some code
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
 }
--- double_linked_list/src/main.rs
+++ double_linked_list/src/main.rs
@@ -2,6 +2,9 @@ mod algorithm;
 use algorithm::KolzoDoublyLinkedList;
 
 fn main() {
-    // let mut ll = KolzoDoublyLinkedList::new();
-    // ll.print();
+    let mut ll = KolzoDoublyLinkedList::new();
+    ll.print();
+    ll.append(2);
+    ll.append(3);
+    ll.print();
 }
